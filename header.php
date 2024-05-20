0de931eae7bde9d451027191b5b3d7c5c91c18ef
Thavi <thavilang@gmail.com>
2024-05-20 20:40:53 -0300

    corrigindo a fonte

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(); ?></title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/fontes/stylesheet.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/fontes/stylesheet.css?v=2">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/<?php echo $post->post_name ?>.css">
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/assets/images/favicon.png" type="image/png">
 
